ed810b6f8e1220075538d7d391ad1cbecb30a673
xry0924-admin <1125894096@qq.com>
2024-12-16 08:58:39 +0800

    使用了用友园区的对应数据，创建了对应的数据源csv文件，修改对应java文件，能够识别csv

--- io.openems.edge.simulator/src/io/openems/edge/simulator/datasource/csv/predefined/Source.java
+++ io.openems.edge.simulator/src/io/openems/edge/simulator/datasource/csv/predefined/Source.java
@@ -5,6 +5,21 @@ public enum Source {
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_STANDARD_LOAD_PROFILE("h0-summer-weekday-standard-load-profile.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_PV_PRODUCTION("h0-summer-weekday-pv-production.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_NON_REGULATED_CONSUMPTION("h0-summer-weekday-non-regulated-consumption.csv"), //
+	L411("411.csv"), //
+	L412("412.csv"), //
+	L413("413.csv"), //
+	L421("421.csv"), //
+	L422("422.csv"), //
+	L431("431.csv"), //
+	L441("441.csv"), //
+	L442("442.csv"), //
+	L451("451.csv"), //
+	L452("452.csv"), //
+	L4611("4611.csv"), //
+	L4612("4612.csv"), //
+	L462("462.csv"), //
+	L471("471.csv"), //
+	L472("472.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_PV_PRODUCTION2("h0-summer-weekday-pv-production2.csv");
 
 	public final String filename;
